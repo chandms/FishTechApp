7bf8f058a602e87a0a751c918f8922f2d73f950a
Chandrika <chandrika.mjee@gmail.com>
2022-11-28 17:58:08 -0500

    adding new changes with sound and looping

--- App.js
+++ App.js
@@ -11,6 +11,12 @@ const logo = {
   height: 64
 };
 
+const soundfiles = {
+  sound1: require('./assets/s1.m4a'),
+  sound2: require('./assets/s2.m4a'),
+  sound3: require('./assets/s3.m4a'),
+};
+
 const styles = StyleSheet.create({
    container: {
      flex: 1,
@@ -35,13 +41,12 @@ const styles = StyleSheet.create({
   },
  });
 
- const Sound2 = () =>  {
+const App = () => {
   const [sound, setSound] = React.useState();
 
-  async function playSound() {
+  async function playSound(sFile) {
     console.log('Loading Sound');
-    const { sound } = await Audio.Sound.createAsync( require('./assets/sample.mp3')
-    );
+    const { sound } = await Audio.Sound.createAsync( sFile, { shouldPlay: true, isLooping: true });
     setSound(sound);
 
     console.log('Playing Sound');
@@ -57,57 +62,50 @@ const styles = StyleSheet.create({
       : undefined;
   }, [sound]);
 
-  return (
-    <View style={styles.container2}>
-      <Button title="Play Sound" onPress={playSound} />
-    </View>
-  );
-}
- 
-
- const buttonClick = () => {
-   Alert.alert('Simple Button pressed');
- };
-
-const App = () => (
-  <SafeAreaView style={{flex :1, justifyContent: 'center', margin: 25 }}>
+  return(
+  <SafeAreaView style={{flex :1, justifyContent: 'center', marginLeft: 5, marginTop: 50, marginBottom:20, marginRight: 10 }}>
    <ScrollView>
       <Card style={{ justifyContent: 'center', margin: 10 }}>
          <ImageBackground source={require('./assets/background.png')} resizeMode="cover" style={styles.image}>
          <Card.Content>
-               <Title>Fish Tech App</Title>
+               <Title>Sound Sample 1</Title>
          </Card.Content>
          <Card.Content>
-         <Paragraph>Building FishTech App</Paragraph>
+         <Paragraph>30 bpm</Paragraph>
          </Card.Content>
          <Image source={require('./assets/fish.png')} />
-         {/* <Button
-         title="button1"
-         onPress={buttonClick}
-         /> */}
-         <Sound2></Sound2>
+         <Button title="Play Sound" onPress={() => {playSound(soundfiles.sound1)}} />
          </ImageBackground>
       </Card>
       <Card style={{ justifyContent: 'center', margin: 10 }}>
          <ImageBackground source={require('./assets/background.png')} resizeMode="cover" style={styles.image}>
          <Card.Content>
-               <Title>Fish Tech App</Title>
+               <Title>Sound Sample 2</Title>
          </Card.Content>
          <Card.Content>
-         <Paragraph>Building FishTech App</Paragraph>
+         <Paragraph>40 bpm</Paragraph>
          </Card.Content>
          <Image source={require('./assets/fish.png')} />
-         {/* <Button
-         title="button2"
-         onPress={buttonClick}
-         /> */}
-         <Sound2></Sound2>
+         <Button title="Play Sound" onPress={() => {playSound(soundfiles.sound2)}} />
+         </ImageBackground>
+      </Card>
+      <Card style={{ justifyContent: 'center', margin: 10 }}>
+         <ImageBackground source={require('./assets/background.png')} resizeMode="cover" style={styles.image}>
+         <Card.Content>
+               <Title>Sound Sample 3</Title>
+         </Card.Content>
+         <Card.Content>
+         <Paragraph>60 bpm</Paragraph>
+         </Card.Content>
+         <Image source={require('./assets/fish.png')} />
+         <Button title="Play Sound" onPress={() => {playSound(soundfiles.sound3)}} />
          </ImageBackground>
       </Card>
    </ScrollView>
    </SafeAreaView>
+  )
    
-);
+};
 
 export default App;
 
