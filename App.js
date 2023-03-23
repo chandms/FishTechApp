4366e2b80bf792d50702157002967f39e3faf9c7
Abby <sande302@purdue.edu>
2023-03-23 10:01:14 -0400

    adding all sounds

--- App.js
+++ App.js
@@ -3,8 +3,40 @@ import { Image, View, ScrollView, Dimensions, Text, ImageBackground, StyleSheet,
 import {Card , Title ,Paragraph } from 'react-native-paper';
 import { Audio } from 'expo-av';
 import Slider from '@react-native-community/slider';
-
+import {NavigationContainer, StackActions} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Welcome from './Welcome';
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Slider" component={Slider_Component} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <SafeAreaView style={styles.safe_are_style}>
+    <Welcome/>
+    <Button
+      title="Slider View"
+      onPress={() =>
+        navigation.navigate('Slider')
+      }
+    />
+    </SafeAreaView>
+  );
+};
 
 // based on the selected slider's value => change the sound
 const logo = {
@@ -14,82 +46,55 @@ const logo = {
 };
 
 const soundfiles = {
-  sound1: require('./assets/s1.m4a'),
-  sound2: require('./assets/s2.m4a'),
-  sound3: require('./assets/s3.m4a'),
-  sound30: require('./assets/sound/30bpm.mp3'),
-  sound40: require('./assets/sound/40bpm.mp3'),
-  sound50: require('./assets/sound/50bpm.mp3'),
-  sound60: require('./assets/sound/60bpm.mp3'),
-  sound70: require('./assets/sound/70bpm.mp3'),
-  sound80: require('./assets/sound/80bpm.mp3'),
-  sound90: require('./assets/sound/90bpm.mp3'),
-  sound100: require('./assets/sound/100bpm.mp3'),
-  soundmax: require('./assets/sound/maxbpm.mp3'),
-
-};
+    sound1: require('./assets/s1.m4a'),
+    sound2: require('./assets/s2.m4a'),
+    sound3: require('./assets/s3.m4a'),
+    sound30: require('./assets/sound/30bpm.mp3'),
+    sound40: require('./assets/sound/40bpm.mp3'),
+    sound50: require('./assets/sound/50bpm.mp3'),
+    sound60: require('./assets/sound/60bpm.mp3'),
+    sound70: require('./assets/sound/70bpm.mp3'),
+    sound80: require('./assets/sound/80bpm.mp3'),
+    sound90: require('./assets/sound/90bpm.mp3'),
+    sound100: require('./assets/sound/100bpm.mp3'),
+    soundmax: require('./assets/sound/maxbpm.mp3'),
+  
+  };
 
 const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-   },
-   image: {
-     flex: 1,
-     justifyContent: "center"
-   },
-   text: {
-     color: "white",
-     fontSize: 42,
-     lineHeight: 84,
-     fontWeight: "bold",
-     textAlign: "center",
-     backgroundColor: "#000000c0"
-   },
-   container2: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 10,
-  },
   container_slider: {
     flex: 1,
     padding: 20,
     justifyContent: 'center',
     backgroundColor: '#ecf0f1',
   },
+  safe_are_style :{
+    flex :1, 
+    justifyContent: 'center', 
+    marginLeft: 5, 
+    marginTop: 50, 
+    marginBottom:20, 
+    marginRight: 10
+  }
  });
 
-const App = () => {
+const Slider_Component = ({navigation, route}) => {
   const [sound, setSound] = React.useState();
   const [sliderValue, setSliderValue] = useState(15);
-
-  
+  const [fileSelected, setFileSelected] = React.useState('');
 
   async function playSound(sFile) {
-    console.log('Loading Sound');
+    console.log('Playing Sound '+ sFile);
     const { sound } = await Audio.Sound.createAsync( sFile, { shouldPlay: true, isLooping: true });
     setSound(sound);
+    setFileSelected(sFile);
 
-    console.log('Playing Sound');
     await sound.playAsync();
+
   }
 
-  async function stopSound() { 
-    console.log('Stopping Sound'); 
-    sound.stopAsync(); 
-  } 
-
-  React.useEffect(() => {
-    return sound
-      ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync();
-        }
-      : undefined;
-  }, [sound]);
-
   async function stopSound(){
-    console.log("stopping sound");
+    console.log("stopping sound "+fileSelected);
     if(sound){
       sound.stopAsync();
     }
@@ -98,52 +103,78 @@ const App = () => {
 
   React.useEffect(() => {
         
-        console.log("current slider value", sliderValue);
-        if(sliderValue==30){
-          playSound(soundfiles.sound30);
+    // console.log("current slider value", sliderValue);
+    if(sliderValue==30){
+      playSound(soundfiles.sound30);
+    }
+    else if(sliderValue==40){
+      playSound(soundfiles.sound40);
+    }
+    else if(sliderValue==50){
+      playSound(soundfiles.sound50);
+    }
+    else if(sliderValue==60){
+      playSound(soundfiles.sound60);
+    }
+    else if(sliderValue==70){
+      playSound(soundfiles.sound70);
+    }
+    else if(sliderValue==80){
+      playSound(soundfiles.sound80);
+    }
+    else if(sliderValue==90){
+      playSound(soundfiles.sound90);
+    }
+    else if(sliderValue==100){
+      playSound(soundfiles.sound100);
+    }
+}, [sliderValue]);
+
+  
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound '+fileSelected);
+          sound.unloadAsync();
         }
-        else if(sliderValue==40){
-          playSound(soundfiles.sound40);
-        }
-        else if(sliderValue==50){
-          playSound(soundfiles.sound50);
-        }
-        else if(sliderValue==60){
-          playSound(soundfiles.sound60);
-        }
-        else if(sliderValue==70){
-          playSound(soundfiles.sound70);
-        }
-        else if(sliderValue==80){
-          playSound(soundfiles.sound80);
-        }
-        else if(sliderValue==90){
-          playSound(soundfiles.sound90);
-        }
-        else if(sliderValue==100){
-          playSound(soundfiles.sound100);
-        }
-  }, [sliderValue]);
+      : undefined;
+  }, [sound]);
 
   return(
-  <SafeAreaView style={{flex :1, justifyContent: 'center', marginLeft: 5, marginTop: 50, marginBottom:20, marginRight: 10 }}>
-   <View style={styles.container_slider}>
-        {/*Text to show slider value*/}
-        <Text style={{color: 'black'}}>Value of slider is : {sliderValue}</Text>
-        <Welcome></Welcome>
-        {/*Slider with max, min, step and initial value*/}
-        <Slider
-          maximumValue={100}
-          minimumValue={0}
-          minimumTrackTintColor="#307ecc"
-          maximumTrackTintColor="#000000"
-          step={1}
-          value={sliderValue}
-          onValueChange={(sliderValue) => setSliderValue(sliderValue)}
-        />
-      </View>
-      <Button title="Stop Sound" onPress={() => {stopSound()}} style={{backgroundColor: 'red'}} />
+    
+    <SafeAreaView style={styles.safe_are_style}>
+ <View style={styles.container_slider}>
+      {/*Text to show slider value*/}
+      <Text style={{color: 'black'}}>Value of slider is : {sliderValue}</Text>
+      
+      {/*Slider with max, min, step and initial value*/}
+      <Slider
+        maximumValue={100}
+        minimumValue={0}
+        minimumTrackTintColor="#307ecc"
+        maximumTrackTintColor="#000000"
+        step={1}
+        value={sliderValue}
+        onValueChange={(sliderValue) => setSliderValue(sliderValue)}
+      />
+    </View>
+    
+    <Button title="Stop Sound" onPress={() => {stopSound()}} style={{backgroundColor: 'red'}} />
+ </SafeAreaView>
+
+);
+}
+
+const App = ({navigation, route}) => {
+  
+  return(
+    
+      <SafeAreaView style={{flex :1, justifyContent: 'center', marginLeft: 5, marginTop: 50, marginBottom:20, marginRight: 10 }}>
+   
+      <MyStack></MyStack> 
+    
    </SafeAreaView>
+  
   )
    
 };
